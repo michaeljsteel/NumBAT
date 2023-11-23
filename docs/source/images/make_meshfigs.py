e28bdb613413e104c56bfb1e1d579db5febf064b
Michael Steel <michael.steel@mq.edu.au>
2023-11-24 10:34:00 +1100

    Mesh drawing fixes

--- docs/source/images/make_meshfigs.py
+++ docs/source/images/make_meshfigs.py
@@ -6,13 +6,8 @@ import time
 
 sys.path.append("../../../backend/")
 
-import objects
+import numbat
 import materials
-#import mode_calcs
-#import integration
-import plotting
-from numbattools import join_figs
-from fortran import NumBAT
 
 mat_bkg = materials.make_material("Vacuum")
 mat_air = materials.make_material("Air")
@@ -23,38 +18,7 @@ mat_d = materials.make_material("SiO2_2016_Smith")
 mat_e = materials.make_material("SiO2_2016_Smith")
 
 
-#def join_figs(fn1, fn2, fnout):
-# 
-#    images = [Image.open(x) for x in [fn2, fn1]]
-#
-#    nsz0 = [images[1].size[0], 
-#            int(images[0].size[1]*
-#            images[1].size[0]/
-#            images[0].size[0])]
-#    images[0]=images[0].resize(nsz0)
-#
-#    widths, heights = zip(*(i.size for i in images))
-#
-#    total_width = sum(widths)
-#    max_height = max(heights)
-#
-#    new_im = Image.new('RGB', (total_width, max_height))
-#
-#    x_offset = 0
-#    yoffs  = [0,0]
-#    if images[0].size[1]>images[1].size[1]:
-#        yoffs=[0, int((images[0].size[1]-images[1].size[1])/2)]
-#    else:
-#        yoffs=[int((images[1].size[1]-images[0].size[1])/2), 0]
-#
-#    for i, im in enumerate(images):
-#      new_im.paste(im, (x_offset,yoffs[i]))
-#      x_offset += im.size[0]
-#
-#    new_im.save(fnout)
-#
-
-def do_oneincl():
+def do_oneincl(nbapp):
 #
 # oneincl_mesh.geo
 # 
@@ -63,23 +27,18 @@ def do_oneincl():
     inc_a_x = 1500
     inc_a_y = 750
     # plain rect waveguide
-    wguide1 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rectangular',
+    wguide1 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rectangular',
                             material_bkg=mat_bkg, material_a=mat_a,
                             lc_bkg=.1, lc_refine_1=10, lc_refine_2=10)
     
     
-    wguide2 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'circular',
+    wguide2 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'circular',
                             material_bkg=mat_bkg, material_a=mat_a,
                             lc_bkg=.1, lc_refine_1=10, lc_refine_2=10)
     wguide1.plot_mesh('rect_wg')
     wguide2.plot_mesh('circ_wg')
-    
-    #time.sleep(3)  # to let gmsh finish writing plots
-    #join_figs('rect_wg_mesh_nodes.png', '../../msh_type_lib/1.png', 'rect_wg_mesh_both.png')
-    #join_figs('circ_wg_mesh_nodes.png', '../../msh_type_lib/1_circular.png', 'circ_wg_mesh_both.png')
-  
 
-def do_twoincl():
+def do_twoincl(nbapp):
     #
     # twoincl_mesh.geo
     # 
@@ -93,23 +52,20 @@ def do_twoincl():
     inc_b_y = inc_a_y*2
     incs_y_offset = -400
     two_inc_sep=500
-    wguide1 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rectangular',
+    wguide1 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rectangular',
                                 inc_b_x=inc_b_x, inc_b_y=inc_b_y, two_inc_sep=two_inc_sep,
                                incs_y_offset=incs_y_offset,
                             material_bkg=mat_air, material_a=mat_a,
                             lc_bkg=.1, lc_refine_1=10, lc_refine_2=10)
     # circ inclusions
-    wguide2 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'circular',
+    wguide2 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'circular',
                                 inc_b_x=inc_b_x, inc_b_y=inc_b_y, two_inc_sep=two_inc_sep,
                             material_bkg=mat_a, material_a=mat_a,
                             lc_bkg=.1, lc_refine_1=10, lc_refine_2=10)
     wguide2.plot_mesh('twoincl_circ_wg')
     
-    #time.sleep(5)
-    #join_figs('twoincl_rect_wg_mesh_nodes.png', '../../msh_type_lib/2.png', 'twoincl_rect_wg_mesh_both.png')
-    #join_figs('twoincl_circ_wg_mesh_nodes.png', '../../msh_type_lib/twoincl_circ_wg_mesh_geom.png', 'twoincl_circ_wg_mesh_both.png')
    
-def do_rib():
+def do_rib(nbapp):
     # rib structures inclusions
     unitcell_x = 4000
     unitcell_y = 3000
@@ -124,14 +80,14 @@ def do_rib():
     coat2_x = 70
     coat2_y = 70
 
-    wguide1 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rib',
+    wguide1 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rib',
                                 slab_a_x=slab_a_x, slab_a_y=slab_a_y, 
                             material_bkg=mat_bkg, material_a=mat_a, 
                                 material_b=mat_b,
                             lc_bkg=.1, lc_refine_1=10, lc_refine_2=10, lc_refine_3=10)
     wguide1.plot_mesh('rib_wg')
 
-    wguide2 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rib_coated',
+    wguide2 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rib_coated',
                                 slab_a_x=slab_a_x, slab_a_y=slab_a_y, 
                                 coat_x=coat_x, coat_y=coat_y, 
                             material_bkg=mat_bkg, material_a=mat_a, 
@@ -140,7 +96,7 @@ def do_rib():
     wguide2.plot_mesh('rib_coated_wg')
 
 
-    wguide3 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rib_double_coated',
+    wguide3 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'rib_double_coated',
                                 slab_a_x=slab_a_x, slab_a_y=slab_a_y, 
                                 coat_x=coat_x, coat_y=coat_y, 
                                 coat2_x=coat2_x, coat2_y=coat2_y, 
@@ -151,11 +107,8 @@ def do_rib():
     wguide3.check_mesh()
     wguide3.plot_mesh('rib_double_coated_wg')
 
-    #join_figs('rib_wg_mesh_nodes.png', '../../msh_type_lib/rib.png', 'rib_wg_mesh_both.png')
-    #join_figs('rib_coated_wg_mesh_nodes.png', '../../msh_type_lib/rib_coated.png', 'rib_coated_wg_mesh_both.png')
-    #join_figs('rib_double_coated_wg_mesh_nodes.png', '../../msh_type_lib/rib_double_coated.png', 'rib_double_coated_wg_mesh_both.png')
     
-def do_slot():
+def do_slot(nbapp):
     # rib structures inclusions
     unitcell_x = 4000
     unitcell_y = 3000
@@ -168,7 +121,7 @@ def do_slot():
     inc_b_x = 300  # pillar width
     coat_y = 50  # pillar coat thickness
 
-    wguide1 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'slot',
+    wguide1 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'slot',
                                 slab_a_x=slab_a_x, slab_a_y=slab_a_y, 
                                 inc_b_x=inc_b_x,
                             material_bkg=mat_bkg, material_a=mat_bkg, 
@@ -180,7 +133,7 @@ def do_slot():
     unitcell_x = 4000
     unitcell_y = 1000
 
-    wguide2 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'slot_coated',
+    wguide2 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'slot_coated',
                                 slab_a_x=slab_a_x, slab_a_y=slab_a_y, 
                                 inc_b_x=inc_b_x,
                                 coat_y=coat_y,
@@ -194,12 +147,9 @@ def do_slot():
     wguide1.check_mesh()
     wguide2.check_mesh()
 
-    #join_figs('slot_wg_mesh_nodes.png', '../../msh_type_lib/slot.png', 'slot_wg_mesh_both.png')
-    #join_figs('slot_coated_wg_mesh_nodes.png', '../../msh_type_lib/slot_coated.png', 
-    #          'slot_coated_wg_mesh_both.png')
 
 
-def do_onion():
+def do_onion(nbapp):
 
     # A concentric Bragg fibre
     unitcell_x = 1500
@@ -209,7 +159,7 @@ def do_onion():
     d1=50
     d2=75
 
-    wguide1 = objects.Structure(unitcell_x,d1,unitcell_y,d1,'onion',
+    wguide1 = nbapp.make_structure(unitcell_x,d1,unitcell_y,d1,'onion',
                                 inc_b_x=d2, inc_c_x=d1, inc_d_x=d2, inc_e_x=d1, inc_f_x=d2,
                                 inc_g_x=d1, inc_h_x=d2, inc_i_x=d1, inc_j_x=d2, inc_k_x=d1,
                                 inc_l_x=d2, inc_m_x=d1, inc_n_x=d2, inc_o_x=d1,
@@ -228,7 +178,7 @@ def do_onion():
     d1=20
     d2=127/2.
 
-    wguide2 = objects.Structure(unitcell_x,d1,unitcell_y,d1,'onion2',
+    wguide2 = nbapp.make_structure(unitcell_x,d1,unitcell_y,d1,'onion2',
                                 inc_b_x=d2, 
                             material_bkg=mat_bkg, material_a=mat_a, material_b=mat_b,
                             lc_bkg=.05, lc_refine_1=5, lc_refine_2=5, lc_refine_3=5)
@@ -243,7 +193,7 @@ def do_onion():
     d2=127/2.
     d3=20
 
-    wguide3 = objects.Structure(unitcell_x,d1,unitcell_y,d1,'onion3',
+    wguide3 = nbapp.make_structure(unitcell_x,d1,unitcell_y,d1,'onion3',
                                 inc_b_x=d2, 
                                 inc_c_x=d3, 
                             material_bkg=mat_bkg, material_a=mat_a, 
@@ -252,13 +202,10 @@ def do_onion():
     wguide3.plot_mesh('onion3_wg')
 
 
-    join_figs('onion_wg_mesh_nodes.png', '../../msh_type_lib/onion.png', 'onion_wg_mesh_both.png')
-    join_figs('onion2_wg_mesh_nodes.png', '../../msh_type_lib/onion2_wg_mesh_geom.png', 'onion2_wg_mesh_both.png')
-    join_figs('onion3_wg_mesh_nodes.png', '../../msh_type_lib/onion3_wg_mesh_geom.png', 'onion3_wg_mesh_both.png')
 
     unitcell_x = 1500
     unitcell_y = 1500
-    wguide1 = objects.Structure(unitcell_x,d1,unitcell_y,d1,'circ_onion',
+    wguide1 = nbapp.make_structure(unitcell_x,d1,unitcell_y,d1,'circ_onion',
                                 inc_b_x=d2, inc_c_x=d1, inc_d_x=d2, inc_e_x=d1, inc_f_x=d2,
                                 inc_g_x=d1, inc_h_x=d2, inc_i_x=d1, inc_j_x=d2, inc_k_x=d1,
                                 inc_l_x=d2, inc_m_x=d1, inc_n_x=d2, inc_o_x=d1,
@@ -277,13 +224,13 @@ def do_onion():
     d1=20
     d2=127/2.
 
-    wguide2 = objects.Structure(unitcell_x,d1,unitcell_y,d1,'circ_onion2',
+    wguide2 = nbapp.make_structure(unitcell_x,d1,unitcell_y,d1,'circ_onion2',
                                 inc_b_x=d2, 
                             material_bkg=mat_bkg, material_a=mat_a, material_b=mat_b,
                             lc_bkg=.05, lc_refine_1=5, lc_refine_2=5, lc_refine_3=5)
     wguide2.plot_mesh('circ_onion2_wg')
 
-    wguide3 = objects.Structure(unitcell_x,d1,unitcell_y,d1,'circ_onion3',
+    wguide3 = nbapp.make_structure(unitcell_x,d1,unitcell_y,d1,'circ_onion3',
                                 inc_b_x=d2, 
                                 inc_c_x=d3, 
                             material_bkg=mat_bkg, material_a=mat_a, 
@@ -291,7 +238,7 @@ def do_onion():
                             lc_bkg=.05, lc_refine_1=5, lc_refine_2=5, lc_refine_3=5)
     wguide3.plot_mesh('circ_onion3_wg')
 
-def do_trapezoid():
+def do_trapezoid(nbapp):
     unitcell_x = 2000
     unitcell_y = 2000
     # layer thicknesses
@@ -300,17 +247,15 @@ def do_trapezoid():
     slab_a_x = 1000
     slab_a_y = 300
 
-    wguide3 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'trapezoidal_rib',
+    wguide3 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'trapezoidal_rib',
                                 slab_a_x=slab_a_x, slab_a_y=slab_a_y,
                             material_bkg=mat_bkg, material_a=mat_a, 
                                 material_b=mat_b,
                             lc_bkg=.05, lc_refine_1=10, lc_refine_2=10)
     wguide3.plot_mesh('trapezoidal_rib_wg')
     #wguide3.check_mesh()
-    join_figs('trapezoidal_rib_wg_mesh_nodes.png', '../../msh_type_lib/trapezoidal_rib_wg_mesh_geom.png', 'trapezoidal_rib_wg_mesh_both.png')
 
-
-def do_pedestal():
+def do_pedestal(nbapp):
     unitcell_x = 3000
     unitcell_y = 2000
 
@@ -326,24 +271,24 @@ def do_pedestal():
     slab_a_y = 300
 
 
-    wguide1 = objects.Structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'pedestal',
+    wguide1 = nbapp.make_structure(unitcell_x,inc_a_x,unitcell_y,inc_a_y,'pedestal',
                                 inc_b_x = inc_b_x, slab_a_x=slab_a_x, slab_a_y=slab_a_y,
                             material_bkg=mat_bkg, material_a=mat_a, material_b=mat_b,
                             pillar_x=pillar_x, pillar_y=pillar_y,
                             lc_bkg=.1, lc_refine_1=10, lc_refine_2=10)
     wguide1.plot_mesh('pedestal_wg')
-    #wguide3.check_mesh()
-    join_figs('pedestal_wg_mesh_nodes.png', '../../msh_type_lib/pedestal_wg_mesh_geom.png', 
-              'pedestal_wg_mesh_both.png')
 
 def do_main():
-    #do_oneincl()
-    #do_twoincl()
-    #do_rib()
-    #do_slot()
-    do_onion()
-    #do_trapezoid()
-    #do_pedestal()
+
+    nbapp = numbat.NumBATApp()
+
+    #do_oneincl(nbapp)
+    do_twoincl(nbapp)
+    #do_rib(nbapp)
+    #do_slot(nbapp)
+    #do_onion(nbapp)
+    #do_trapezoid(nbapp)
+    #do_pedestal(nbapp)
 
 
 if __name__=='__main__':
