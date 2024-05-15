6a107c0b48c406db5ad3c9c3dae6e3be54c5f758
Michael Steel <michael.steel@mq.edu.au>
2024-05-15 17:54:51 +1000

    tt

--- tutorials/simo-tut_17-rotations.py
+++ tutorials/simo-tut_17-rotations.py
@@ -23,17 +23,18 @@ inc_a_y = inc_a_x
 inc_shape = 'circular'
 
 mat_a=materials.make_material("SiO2_2021_Poulton")        # isotropic
-mat_b=materials.make_material("Si_2012_Rakich")  # cubic
+#mat_b=materials.make_material("Si_2012_Rakich")  # cubic
+mat_b=materials.make_material("Ge_cubic_2014_Wolff")  # cubic
 mat_c=materials.make_material("LiNbO3aniso_2021_Steel") # tetragonal
 
 mat_d=materials.make_material("LiNbO3aniso_2021_Steel") # tetragonal
 
 pref = 'tut_17'
 
-mat_as = [copy.copy(mat_a) for i in range(4)]
-mat_bs = [copy.copy(mat_b) for i in range(4)]
-mat_cs = [copy.copy(mat_c) for i in range(4)]
-mat_ds = [copy.copy(mat_d) for i in range(4)]
+mat_as = [copy.deepcopy(mat_a) for i in range(4)]
+mat_bs = [copy.deepcopy(mat_b) for i in range(4)]
+mat_cs = [copy.deepcopy(mat_c) for i in range(4)]
+mat_ds = [copy.deepcopy(mat_d) for i in range(4)]
 
 #print('x pi/4')
 mat_as[0].rotate(pi/5, 'x-axis')
@@ -48,20 +49,20 @@ mat_as[0].rotate(pi/5, 'x-axis')
 mat_bs[0].rotate(pi/2, 'x-axis')
 mat_bs[1].rotate(-pi/2, 'y-axis')
 mat_bs[2].rotate(pi/4, (1,1,1))
-mat_bs[3].rotate(pi/4, np.array([1,1,1]))
+mat_bs[3].rotate(pi/3, np.array([1,1,1]))
 
-mat_cs[0].rotate(pi/2, 'x-axis')
+#mat_cs[0].rotate(pi/2, 'x-axis')
 mat_cs[1].rotate(-pi/2, 'y-axis')
 mat_cs[2].rotate(pi/4, (1,1,1))
 mat_cs[3].rotate(pi/4, np.array([1,2,1]))
 
-mat_ds[0].set_orientation('x-cut')
-mat_ds[1].set_orientation('y-cut')
-mat_ds[2].set_orientation('y-cut')
-mat_ds[2].set_orientation('z-cut')
-mat_ds[3].set_orientation('y-cut')
-mat_ds[3].set_orientation('z-cut')
-mat_ds[3].set_orientation('x-cut')
+#mat_ds[0].set_orientation('x-cut')
+#mat_ds[1].set_orientation('y-cut')
+#mat_ds[2].set_orientation('y-cut')
+#mat_ds[2].set_orientation('z-cut')
+#mat_ds[3].set_orientation('y-cut')
+#mat_ds[3].set_orientation('z-cut')
+#mat_ds[3].set_orientation('x-cut')
 
 
 
@@ -72,8 +73,9 @@ mat_ds[3].set_orientation('x-cut')
 #        m.make_crystal_plot(pref+f'{im:d}')
 #    #for m in mats: m.plot_bulk_dispersion(pref)
 
-for mats in (mat_ds,):
-    for im,m in enumerate(mats): 
-        m.plot_bulk_dispersion(pref+f'-{im:d}')
+print(mat_cs[0].c_tensor.mat)
+for am,mats in enumerate((mat_cs,)):
+    for im,m in enumerate((mats[0],)): 
+        m.plot_bulk_dispersion(pref+f'-{am:d}-{im:d}')
 
 
