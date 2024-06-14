8306ef28f85e337ea7891bc91725dfd0d9c6dd14
Michael Steel <michael.steel@mq.edu.au>
2024-06-14 17:21:05 +1000

    closer to finding AC seg fault

--- backend/fortran/src/py_calc_modes_impl.f90
+++ backend/fortran/src/py_calc_modes_impl.f90
@@ -1,6 +1,5 @@
 #include "numbat_decl.h"
 
-
 subroutine prepare_workspaces(is_em, n_msh_pts, n_msh_el, n_modes, &
    int_max, cmplx_max, real_max, &
    a_iwork, b_zwork, c_dwork, d_dwork, iindex, overlap_L, &
@@ -26,7 +25,8 @@ subroutine prepare_workspaces(is_em, n_msh_pts, n_msh_el, n_modes, &
    integer :: stat=0
 
    write(*,*) 'pw1', n_msh_pts, n_msh_el
-   call array_size(n_msh_pts, n_msh_el, n_modes, int_max, cmplx_max, real_max, n_ddl, errco, emsg)
+   call array_size(n_msh_pts, n_msh_el, n_modes, &
+      int_max, cmplx_max, real_max, n_ddl, errco, emsg)
    RETONERROR(errco)
 
    write(*,*) 'pw1b', int_max, cmplx_max, n_ddl
@@ -51,79 +51,79 @@ subroutine prepare_workspaces(is_em, n_msh_pts, n_msh_el, n_modes, &
 
    write(*,*) 'pw4'
    if (is_em > 0) then
-       allocate(d_dwork(2,n_ddl), STAT=stat)
-       call check_alloc(stat, 2*n_ddl, "d_dwork", -1, errco, emsg)
-       RETONERROR(errco)
+      allocate(d_dwork(2,n_ddl), STAT=stat)
+      call check_alloc(stat, 2*n_ddl, "d_dwork", -1, errco, emsg)
+      RETONERROR(errco)
 
-       allocate(overlap_L(n_modes,n_modes), STAT=stat)
-       call check_alloc(stat, n_modes*n_modes, "overlap_L", -1, errco, emsg)
-       RETONERROR(errco)
+      allocate(overlap_L(n_modes,n_modes), STAT=stat)
+      call check_alloc(stat, n_modes*n_modes, "overlap_L", -1, errco, emsg)
+      RETONERROR(errco)
 
 
    endif
-write(*,*) 'pw6'
+   write(*,*) 'pw6'
 
 end subroutine prepare_workspaces
 
 !   ----------------------------------------------------------------------------------------
 
 subroutine set_boundary_conditions(bdy_cdn, n_msh_pts, n_msh_el, mesh_xy, nodes_per_el, &
-        type_nod, table_nod, n_ddl, neq, ip_type_N_E_F, ip_eq, &
-        a_iwork, d_dwork, int_max, debug)
+   type_nod, table_nod, n_ddl, neq, ip_type_N_E_F, ip_eq, &
+   a_iwork, d_dwork, int_max, debug)
 
-    use numbatmod
+   use numbatmod
 
-    integer*8 :: bdy_cdn, neq, n_msh_pts, n_msh_el, n_ddl, nodes_per_el
-    integer*8 :: ip_type_N_E_F, ip_eq, jp_x_n_e_f, int_max
-    integer*8 :: ip_period_N, ip_nperiod_N, ip_period_N_E_F, ip_nperiod_N_E_F
-    integer*8 :: debug
-    double precision mesh_xy(2,n_msh_pts)
-    integer*8 type_nod(n_msh_pts)
-    integer*8 table_nod(nodes_per_el, n_msh_el)
+   integer*8 :: bdy_cdn, neq, n_msh_pts, n_msh_el, n_ddl, nodes_per_el
+   integer*8 :: ip_type_N_E_F, ip_eq, jp_x_n_e_f, int_max
+   integer*8 :: ip_period_N, ip_nperiod_N, ip_period_N_E_F, ip_nperiod_N_E_F
+   integer*8 :: debug
+   double precision mesh_xy(2,n_msh_pts)
+   integer*8 type_nod(n_msh_pts)
+   integer*8 table_nod(nodes_per_el, n_msh_el)
 
-    ! is this the right way to pass these?
-    integer*8, dimension(int_max) :: a_iwork
-    double precision, dimension(2,n_ddl) :: d_dwork
+   ! is this the right way to pass these?
+   integer*8, dimension(int_max) :: a_iwork
+   double precision, dimension(2,n_ddl) :: d_dwork
 
-    double precision, dimension(2,2) :: lat_vecs
+   double precision, dimension(2,2) :: lat_vecs
 
-    if ( bdy_cdn .eq. BCS_DIRICHLET .or.  bdy_cdn .eq. BCS_NEUMANN) then
+   if ( bdy_cdn .eq. BCS_DIRICHLET .or.  bdy_cdn .eq. BCS_NEUMANN) then
 
-        call bound_cond ( bdy_cdn, n_ddl, neq, a_iwork(ip_type_N_E_F), a_iwork(ip_eq))
+      call bound_cond ( bdy_cdn, n_ddl, neq, a_iwork(ip_type_N_E_F), a_iwork(ip_eq))
 
-    elseif( bdy_cdn .eq. BCS_PERIODIC) then  !  Periodic  conditions (never in NumBAT)
-        if (debug .eq. 1) then
-            write(*,*) "###### periodic_node"
-        endif
+   elseif( bdy_cdn .eq. BCS_PERIODIC) then  !  Periodic  conditions (never in NumBAT)
+      if (debug .eq. 1) then
+         write(*,*) "###### periodic_node"
+      endif
 
-        ! reproduced in py_calc_modes.f
-        jp_x_N_E_F = 1
-        ip_period_N = ip_type_N_E_F + 2*n_ddl
-        ip_nperiod_N = ip_period_N + n_msh_pts
-        ip_period_N_E_F = ip_nperiod_N + n_msh_pts
-        ip_nperiod_N_E_F = ip_period_N_E_F + n_ddl
-        ip_eq = ip_nperiod_N_E_F + n_ddl
+      ! reproduced in py_calc_modes.f
+      jp_x_N_E_F = 1
+      ip_period_N = ip_type_N_E_F + 2*n_ddl
+      ip_nperiod_N = ip_period_N + n_msh_pts
+      ip_period_N_E_F = ip_nperiod_N + n_msh_pts
+      ip_nperiod_N_E_F = ip_period_N_E_F + n_ddl
+      ip_eq = ip_nperiod_N_E_F + n_ddl
 
-        call lattice_vec (n_msh_pts, mesh_xy, lat_vecs, debug)
+      call lattice_vec (n_msh_pts, mesh_xy, lat_vecs, debug)
 
-        call periodic_node(n_msh_el, n_msh_pts, nodes_per_el, type_nod, mesh_xy, a_iwork(ip_period_N), &
-            a_iwork(ip_nperiod_N), table_nod, lat_vecs)
+      call periodic_node(n_msh_el, n_msh_pts, nodes_per_el, type_nod, mesh_xy, a_iwork(ip_period_N), &
+         a_iwork(ip_nperiod_N), table_nod, lat_vecs)
 
-        if (debug .eq. 1) then
-            write(*,*) "set_boundary_conditions: ###### periodic_N_E_F"
-        endif
+      if (debug .eq. 1) then
+         write(*,*) "set_boundary_conditions: ###### periodic_N_E_F"
+      endif
 
-        call periodic_N_E_F (n_ddl, a_iwork(ip_type_N_E_F), d_dwork, a_iwork(ip_period_N_E_F), &
-            a_iwork(ip_nperiod_N_E_F), lat_vecs)
+      call periodic_N_E_F (n_ddl, a_iwork(ip_type_N_E_F), d_dwork, a_iwork(ip_period_N_E_F), &
+         a_iwork(ip_nperiod_N_E_F), lat_vecs)
 
-        call periodic_cond ( bdy_cdn, n_ddl, neq, a_iwork(ip_type_N_E_F), &
-            a_iwork(ip_period_N_E_F), a_iwork(ip_eq), debug)
+      call periodic_cond ( bdy_cdn, n_ddl, neq, a_iwork(ip_type_N_E_F), &
+         a_iwork(ip_period_N_E_F), a_iwork(ip_eq), debug)
 
-        if (debug .eq. 1) then
-            write(*,*) "py_calc_modes.f: neq, n_ddl = ", neq, n_ddl
-        endif
-        write(*,*) "py_calc_modes.f: neq, n_ddl = ", neq, n_ddl
+      if (debug .eq. 1) then
+         write(*,*) "py_calc_modes.f: neq, n_ddl = ", neq, n_ddl
+      endif
+      write(*,*) "py_calc_modes.f: neq, n_ddl = ", neq, n_ddl
 
-    endif
+   endif
 
 end subroutine set_boundary_conditions
