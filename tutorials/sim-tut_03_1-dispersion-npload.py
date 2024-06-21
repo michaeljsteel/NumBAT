a917ce99abbbd30df9806cdf523ec9e93e3c546b
Michael Steel <michael.steel@mq.edu.au>
2024-06-21 14:56:59 +1000

    tut 3 working

--- tutorials/sim-tut_03_1-dispersion-npload.py
+++ tutorials/sim-tut_03_1-dispersion-npload.py
@@ -47,38 +47,45 @@ n_eff = wguide.get_material('a').refindex_n-0.1
 
 # Assuming this calculation is run directly after sim-tut_02
 # we don't need to recalculate EM modes, but can load them in.
-sim_EM_pump = mode_calcs.load_simulation('tut02_wguide_data')
-sim_EM_Stokes = mode_calcs.load_simulation('tut02_wguide_data2')
+#sim_EM_pump = mode_calcs.load_simulation('tut02_wguide_data')
+#sim_EM_Stokes = mode_calcs.load_simulation('tut02_wguide_data2')
+
+simres_EM_pump = wguide.calc_EM_modes(num_modes_EM_pump, lambda_nm, n_eff)
+simres_EM_Stokes = mode_calcs.bkwd_Stokes_modes(simres_EM_pump)
+
 
 # Will scan from forward to backward SBS so need to know q_AC of backward SBS.
-q_AC = np.real(sim_EM_pump.kz_EM(0) - sim_EM_Stokes.kz_EM(0))
+q_AC = np.real(simres_EM_pump.kz_EM(0) - simres_EM_Stokes.kz_EM(0))
 
 # Number of wavevector steps.
 nu_ks = 20
 
 fig, ax = plt.subplots()
+symmetries_working = False
 for i_ac, q_ac in enumerate(np.linspace(0.0, q_AC, nu_ks)):
-    sim_AC = wguide.calc_AC_modes(num_modes_AC, q_ac, EM_sim=sim_EM_pump)
+    sim_AC = wguide.calc_AC_modes(num_modes_AC, q_ac, EM_sim=simres_EM_pump)
     prop_AC_modes = np.array(
         [np.real(x) for x in sim_AC.nu_AC_all() if abs(np.real(x)) > abs(np.imag(x))])
-    sym_list = integration.symmetries(sim_AC)
 
-    for i in range(len(prop_AC_modes)):
-        Om = prop_AC_modes[i]*1e-9
-        if sym_list[i][0] == 1 and sym_list[i][1] == 1 and sym_list[i][2] == 1:
-            sym_A, = ax.plot(np.real(q_ac/q_AC), Om, 'or')
-        if sym_list[i][0] == -1 and sym_list[i][1] == 1 and sym_list[i][2] == -1:
-            sym_B1, = ax.plot(np.real(q_ac/q_AC), Om, 'vc')
-        if sym_list[i][0] == 1 and sym_list[i][1] == -1 and sym_list[i][2] == -1:
-            sym_B2, = ax.plot(np.real(q_ac/q_AC), Om, 'sb')
-        if sym_list[i][0] == -1 and sym_list[i][1] == -1 and sym_list[i][2] == 1:
-            sym_B3, = ax.plot(np.real(q_ac/q_AC), Om, '^g')
+    if symmetries_working:
+        sym_list = integration.symmetries(sim_AC)
+        for i in range(len(prop_AC_modes)):
+            Om = prop_AC_modes[i]*1e-9
+            if sym_list[i][0] == 1 and sym_list[i][1] == 1 and sym_list[i][2] == 1:
+                sym_A, = ax.plot(np.real(q_ac/q_AC), Om, 'or')
+            if sym_list[i][0] == -1 and sym_list[i][1] == 1 and sym_list[i][2] == -1:
+                sym_B1, = ax.plot(np.real(q_ac/q_AC), Om, 'vc')
+            if sym_list[i][0] == 1 and sym_list[i][1] == -1 and sym_list[i][2] == -1:
+                sym_B2, = ax.plot(np.real(q_ac/q_AC), Om, 'sb')
+            if sym_list[i][0] == -1 and sym_list[i][1] == -1 and sym_list[i][2] == 1:
+                sym_B3, = ax.plot(np.real(q_ac/q_AC), Om, '^g')
 
     print("Wavevector loop", i_ac+1, "/", nu_ks)
 
 ax.set_ylim(0, 25)
 ax.set_xlim(0, 1)
-ax.legend([sym_A, sym_B1, sym_B2, sym_B3], [
+if symmetries_working:
+    ax.legend([sym_A, sym_B1, sym_B2, sym_B3], [
           'A', r'B$_1$', r'B$_2$', r'B$_3$'], loc='lower right')
 ax.set_xlabel(r'Normalised axial wavevector $q/(2\beta)$')
 ax.set_ylabel(r'Frequency $\Omega/(2\pi)$ [GHz]')
