ce418a39e7ed4926e174e301776065af80c9e818
Michael Steel <michael.steel@mq.edu.au>
2024-07-01 09:48:10 +1000

    clean on linux

--- backend/fortran/src/pyinterfaces.f90
+++ backend/fortran/src/pyinterfaces.f90
@@ -4,13 +4,11 @@ subroutine conv_gmsh(geoname, assertions_on, errco, emsg)
    use numbatmod
 
    !TODO: f2py doesn't seem to be able to handle this length being set by the #include and preprocessor
-   integer :: assertions_on, errco
-
+   integer :: assertions_on
    character(len=FNAME_LENGTH) :: geoname
-   character(len=EMSG_LENGTH) :: emsg
 
-!f2py intent(in) geoname, assertions_on
-!f2py intent(out) errco, emsg
+   integer, intent(out) :: errco
+   character(len=EMSG_LENGTH), intent(out) :: emsg
 
    errco = 0
    call conv_gmsh_impl(geoname, assertions_on, errco, emsg)
