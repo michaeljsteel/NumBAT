0e547db10d80a1ef7731c81db5a17fb2a868b9df
Michael Steel <michael.steel@mq.edu.au>
2024-07-08 22:46:28 +1000

    add command launcher for windows

---
+++ share/numbat_cmd.bat
@@ -0,0 +1,11 @@
+
+@echo Starting NumBAT development terminal
+@echo.
+
+@echo Activating Python environment for numbat
+@call "%HOMEPATH%\AppData\Local\anaconda3\condabin\conda.bat" activate nbpy3
+
+@echo.
+@call "c:\Program Files (x86)\Intel\oneAPI\setvars.bat"
+
+
