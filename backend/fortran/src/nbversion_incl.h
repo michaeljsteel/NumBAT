4d0bcfeb50147c8410029e67db9d96425701d06e
Michael Steel <michael.steel@mq.edu.au>
2026-02-02 16:57:47 +1100

    version tick

--- backend/fortran/src/nbversion_incl.h
+++ backend/fortran/src/nbversion_incl.h
@@ -1,5 +1,5 @@
-character(len=100), parameter :: NUMBAT_VERSION_STR_MMM = "2.1.5"
+character(len=100), parameter :: NUMBAT_VERSION_STR_MMM = "2.1.6"
 character(len=100), parameter :: NUMBAT_VERSION_STR_MM = "2.1"
 integer(8), parameter :: NUMBAT_VERSION_MAJ = 2
 integer(8), parameter :: NUMBAT_VERSION_MIN = 1
-integer(8), parameter :: NUMBAT_VERSION_MIC = 5
+integer(8), parameter :: NUMBAT_VERSION_MIC = 6
